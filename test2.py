8bfa6d275a3224307c58675b983e92ea0b89be8d
Oleksandr Slovak <slovak194@gmail.com>
2019-01-31 00:40:52 +0100

    Update test2

--- test2.py
+++ test2.py
@@ -22,8 +22,9 @@ th = get_thrusters_poses("../bluerov_ffg/urdf/brov2.xacro")
 
 F_b_summ = sp.Matrix.zeros(6, 1)
 
+th_f_x = sp.symarray('th_f_x', (len(th.keys()), 1))
 
-for k in th.keys():
+for n, k in enumerate(th.keys()):
     th[k]["T_b_t"] = sp.Matrix(th[k]["T_b_t"])
 
     # R = th[k]["T_b_t"][0:3, 0:3]
@@ -35,7 +36,7 @@ for k in th.keys():
     so3_bt = sophus.So3(R)
     se3_bt = sophus.Se3(so3_bt, p)
 
-    F_t = sp.Matrix([sp.Matrix([0, 0, 0]), sp.Matrix([sp.symbols(k + '_fx_t'), 0, 0])])
+    F_t = sp.Matrix([sp.Matrix([0, 0, 0]), sp.Matrix([th_f_x[n, 0], 0, 0])])
     F_b = sophus.Se3.Adj(se3_bt.inverse()).T * F_t
 
     F_b_summ += F_b
@@ -53,6 +54,7 @@ m = sp.symbols('m')
 
 
 V_b = sp.Matrix([sp.Matrix(sp.symarray('w', (3, 1))), sp.Matrix(sp.symarray('v', (3, 1)))])
+dV_b = sp.Matrix([sp.Matrix(sp.symarray('dw', (3, 1))), sp.Matrix(sp.symarray('dv', (3, 1)))])
 
 AdV_b = adv(V_b)
 
@@ -60,17 +62,16 @@ G_b = sp.Matrix.zeros(6, 6)
 G_b[0:3, 0:3] = I_b
 G_b[3:, 3:] = m * sp.Matrix.eye(3)
 
+
+expr = G_b * dV_b - AdV_b.T * G_b * V_b - F_b_summ
+
+
 # F_b = G_b * dV_b - [AdV_b].T * G_b * V_b
 
-exit(0)
 
 # Have effort for all the thrusters
 # Create wrenches for all thr
 # Express wrenches in s frame
 # Summ all wrenches in s frame
 # Do forward synamics
-
-
-
-V = sp.Matrix([1, 2, 3, 4, 5, 6])
 
