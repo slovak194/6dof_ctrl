691c2625921bde769e142bc7b27572b8189d3485
Oleksandr Slovak <olslo@vestas.com>
2019-02-11 15:28:41 +0100

    + optimal turn

--- test_pybullet.py
+++ test_pybullet.py
@@ -10,8 +10,8 @@ import quaternion as nq
 
 from pyxacro import get_robot
 
-physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
-p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
+p.setAdditionalSearchPath(pybullet_data.getDataPath())  # optionally
 p.setGravity(0, 0, 0)
 planeId = p.loadURDF("plane.urdf")
 cubeStartPos = [0, 0, 1]
@@ -19,47 +19,52 @@ cubeStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
 
 urdf_file_path = "../bluerov_ffg/urdf/brov2.urdf"
 
-
 brov2 = get_robot(urdf_file_path)
-
 
 boxId = p.loadURDF(urdf_file_path, cubeStartPos, cubeStartOrientation, flags=p.URDF_USE_INERTIA_FROM_FILE)
 
-
-h = np.array([
+k = np.array([
     brov2["robot"]["link"][0]["inertial"]["inertia"]["@ixx"],
     brov2["robot"]["link"][0]["inertial"]["inertia"]["@iyy"],
     brov2["robot"]["link"][0]["inertial"]["inertia"]["@izz"]
 ], dtype=np.float64)
 
-k = h
+h = k * 10
 
-q_target = from_euler_angles(np.pi/6, 0, 0)
+q_st = from_euler_angles(np.pi / 1.1,
+                         np.pi / 3,
+                         0)
 
-for i in range(10000):
+q_ts = q_st.conj()
 
-    t, q_self = p.getBasePositionAndOrientation(boxId)
-    t = np.array(t)
-    q_self = nq.quaternion(*q_self)
-    v, w = p.getBaseVelocity(boxId)
-    v = np.array(v)
-    w = np.array(w)
+p.addUserDebugLine([0, 0, 0], [1, 0, 0], [1, 0, 0], parentObjectUniqueId=boxId, parentLinkIndex=-1)
+p.addUserDebugLine([0, 0, 0], [0, 1, 0], [0, 1, 0], parentObjectUniqueId=boxId, parentLinkIndex=-1)
+p.addUserDebugLine([0, 0, 0], [0, 0, 1], [0, 0, 1], parentObjectUniqueId=boxId, parentLinkIndex=-1)
 
-    q_diff = q_target.conj() * q_self
+for i in range(1000000):
 
-    M = -h*w - k*q_diff.w*q_diff.vec
-    #
-    # k_i = I_i
-    # h_i = I_i
+    t_sb, q_sb = p.getBasePositionAndOrientation(boxId)
+    t_sb = np.array(t_sb)
+    q_sb = nq.quaternion(q_sb[3], q_sb[0], q_sb[1], q_sb[2])
+    R_bs = as_rotation_matrix(q_sb).T
+    v_s, w_s = p.getBaseVelocity(boxId)
+    v_s = np.array(v_s)
+    w_s = np.array(w_s)
 
-    # p.applyExternalForce(boxId, 4, [0, 0, 40], [0, 0, 0], flags=p.LINK_FRAME)
-    # p.applyExternalForce(boxId, 5, [0, 0, 40], [0, 0, 0], flags=p.LINK_FRAME)
-    p.applyExternalTorque(boxId, 4, M, flags=p.LINK_FRAME)
+    w_b = R_bs @ w_s
+    v_b = R_bs @ v_s
+
+    q_tb = q_ts * q_sb
+
+    M_b = -h * w_b - k * q_tb.w * q_tb.vec
+
+    p.applyExternalTorque(boxId, -1, M_b, flags=p.WORLD_FRAME)  # TODO bug in pybullet. WORLD_FRAME and link frame are inverted https://github.com/bulletphysics/bullet3/issues/1949
 
     p.stepSimulation()
 
     # time.sleep(1./240.)
+    if i % 10 == 0:
+        time.sleep(1. / 1000000.0)
 
-cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
-print(cubePos, cubeOrn)
+
 p.disconnect()
