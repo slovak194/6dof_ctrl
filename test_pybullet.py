3c5e625f1a809fd1c06762e0c79d1dc2cc80a733
Oleksandr Slovak <slovak194@gmail.com>
2019-02-13 21:31:52 +0100

    + video

--- test_pybullet.py
+++ test_pybullet.py
@@ -23,6 +23,9 @@ get_ftz_from_F_c = get_get_ftz_from_F_c()["lambda"]
 physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath())  # optionally
 p.setGravity(0, 0, 0)
+
+p.startStateLogging(p.STATE_LOGGING_VIDEO_MP4, "brov2.mp4")
+
 planeId = p.loadURDF("plane.urdf")
 cubeStartPos = [0, 0, 1]
 cubeStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
