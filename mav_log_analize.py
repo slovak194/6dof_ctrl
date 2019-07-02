2c1b0a58eb5fe6be8e2120caf48906c8a7c30e3a
Oleksandr Slovak <slovak194@gmail.com>
2019-07-02 11:20:34 +0200

    log analizer update

--- mav_log_analize.py
+++ mav_log_analize.py
@@ -1,6 +1,9 @@
 # %%
 import os
 import sys
+
+import json
+
 import pandas as pd
 
 from plotly import tools
@@ -8,35 +11,65 @@ import plotly.graph_objs as go
 
 from webplot import WebPlot
 
+Ts = 0.04  # Most probably
+
 path = "./"
 
+#
+# "log_13_2019-7-1-20-34-24.bin",
+# "log_14_2019-7-2-09-20-34.bin",
+
 bin_log_names = [
-    "log_13_2019-7-1-20-34-24.bin",
-    "log_13_2019-7-1-20-34-25.bin"
+    "log_1_2019-7-2-10-21-48.bin"
 ]
+
+log_keys = [bin_log_name.split(".")[0] for bin_log_name in bin_log_names]
 
 msg_types = [
     "ATT",
     "IMU"
 ]
 
-din = {}
+data_selected = {}
+data_json = {}
 min_ts = sys.maxsize
 
+
+def file_n_lines(file_path):
+    return len([l for l in open(file_path, 'r')])
+
+
 for bin_log_name in bin_log_names:
-    din[bin_log_name.split(".")[0]] = {}
-    for msg_type in msg_types:
+    log_key = bin_log_name.split(".")[0]
+
+    data_selected[log_key] = {}
+    data_json[log_key] = {}
+
+    bin_log_path = path + bin_log_name
+    json_log_path = path + bin_log_name + "." + "json"
+
+    os.system("mavlogdump.py --format=json " + bin_log_path + " > " + json_log_path)
+
+    data_json[log_key]["data"] = [json.loads(line) for line in open(json_log_path, 'r')]
+    data_json[log_key]["messages"] = sorted(set([v["meta"]["type"] for v in data_json[log_key]["data"]]))
+
+    print(data_json[log_key]["messages"])
+
+    for msg_type in data_json[log_key]["messages"]:
         csv_log_path = path + bin_log_name + "." + msg_type + ".csv"
-        bin_log_path = path + bin_log_name
         os.system("mavlogdump.py --format=csv --types="
                   + msg_type + " " + bin_log_path + " > " + csv_log_path)
 
-        din[bin_log_name.split(".")[0]][msg_type] = pd.read_csv(csv_log_path, index_col=None, header=0)
+        if file_n_lines(csv_log_path) < 2:
+            continue
 
-        if min_ts > din[bin_log_name.split(".")[0]][msg_type]["TimeUS"][0]:
-            min_ts = din[bin_log_name.split(".")[0]][msg_type]["TimeUS"][0]
+        data_selected[log_key][msg_type] = pd.read_csv(csv_log_path, index_col=None, header=0)
 
-data_prep = din
+        if "TimeUS" in data_selected[log_key][msg_type].keys():
+            if min_ts > data_selected[log_key][msg_type]["TimeUS"][0]:
+                min_ts = data_selected[log_key][msg_type]["TimeUS"][0]
+
+data_prep = data_selected
 
 
 # %%
@@ -74,3 +107,59 @@ for k, v in data_prep.items():
     plt.plot(fig)
 
 plt.show()
+
+
+# %%
+
+# NKF6 -
+# MOTB
+# IMU
+# NKF7
+# RATE
+# NKF9
+# DU32
+# NKF2
+# CTRL
+# PM
+# NKQ2
+# NKF5
+# IMU2
+# PARM
+# AHR2
+# NKQ1
+# CTUN
+# POWR
+# BARO
+# MAG
+# NKF4
+# MSG
+# ATT
+# RCOU
+# VIBE
+# FMT
+# NKF1
+# NKF3
+# NKF8
+# RCIN
+# EV
+# MODE
+
+A = set(data_json["log_13_2019-7-1-20-34-24"]["messages"])
+B = set(data_json["log_14_2019-7-2-09-20-34"]["messages"])
+
+# print(A)
+# print(B)
+
+print(A.difference(B))
+print(B.difference(A))
+
+A.symmetric_difference(B)
+
+
+# %%
+
+from pprint import pprint
+
+msg_struct = {msg_name: {trace_name: '' for trace_name, trace_value in msg_value.items()} for msg_name, msg_value in data_selected[log_keys[0]].items()}
+
+pprint(msg_struct)
